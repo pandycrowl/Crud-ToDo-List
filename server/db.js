4037780b58c4ed81ce344080f1d126c57f77c0e8
pandycrowl <89517333+pandycrowl@users.noreply.github.com>
2022-02-21 09:03:14 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: "postgres",
-    password: "71253andrey",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "crud"
